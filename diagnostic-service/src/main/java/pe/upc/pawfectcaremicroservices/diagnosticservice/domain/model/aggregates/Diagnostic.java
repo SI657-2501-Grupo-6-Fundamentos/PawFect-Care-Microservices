5404ff73f90fd002d1f3e66df96f5be407f4f2f0
rrenzosilva <149326018+rrenzosilva@users.noreply.github.com>
2025-06-21 00:03:08 -0500

    feat: add Diagnostic class

---
+++ diagnostic-service/src/main/java/pe/upc/pawfectcaremicroservices/diagnosticservice/domain/model/aggregates/Diagnostic.java
@@ -0,0 +1,38 @@
+package pe.upc.pawfectcaremicroservices.diagnosticservice.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import pe.upc.pawfectcaremicroservices.diagnosticservice.domain.model.commands.CreateDiagnosticCommand;
+import pe.upc.pawfectcaremicroservices.diagnosticservice.domain.model.valueobjects.DiagnosticType;
+
+import java.time.LocalDateTime;
+
+public class Diagnostic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime diagnosticDate;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private DiagnosticType diagnosticType;
+
+    public Diagnostic() {
+        this.diagnosticDate = LocalDateTime.now();
+        this.description = "";
+        this.diagnosticType = DiagnosticType.GENERAL_MEDICINE;
+    }
+
+    public Diagnostic(CreateDiagnosticCommand command) {
+        this();
+        this.diagnosticDate = command.diagnosticDate();
+        this.description = command.description();
+        this.diagnosticType = DiagnosticType.GENERAL_MEDICINE;
+    }
+
+    public Diagnostic updateInformation(LocalDateTime diagnosticDate, String description, DiagnosticType diagnosticType) {
+        this.diagnosticDate = diagnosticDate;
+        this.description = description;
+        this.diagnosticType = diagnosticType;
+        return this;
+    }
+}
