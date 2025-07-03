b7f72a79ad3342b59018b013827355d48d8c3bdd
Jose Gutierrez <josecitogutigar21@gmail.com>
2025-07-02 23:23:53 -0500

    feat: add swagger to API gateway

--- gateway-service/src/main/java/pe/upc/pawfectcaremicroservices/gatewayservice/infrastructure/security/SecurityConfig.java
+++ gateway-service/src/main/java/pe/upc/pawfectcaremicroservices/gatewayservice/infrastructure/security/SecurityConfig.java
@@ -42,7 +42,10 @@ public class SecurityConfig {
                                 "/diagnostic-service/**",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/swagger-config",
+                                "/webjars/**"
                         ).permitAll()
                         .anyExchange().authenticated()
                 );
